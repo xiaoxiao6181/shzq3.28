390084c536e6ecc1c1e7482bb4015b0f99f681c2
xiaoxiao6181 <167971162+xiaoxiao6181@users.noreply.github.com>
2026-05-12 10:23:32 +0800

    Add files via upload

---
+++ assets/use-route-CmbnKQLT.js
@@ -0,0 +1,1 @@
+import{aB as n}from"./index-CfhAUm6t.js";const s={to:[String,Object],url:String,replace:Boolean};function a({to:e,url:o,replace:t,$router:r}){e&&r?r[t?"replace":"push"](e):o&&(t?location.replace(o):location.href=o)}function i(){const e=n().proxy;return()=>a(e)}export{s as r,i as u};
